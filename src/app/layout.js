1fd5c2f871c7908609e3f8a2c9c990fb822fe016
fahimshariar28 <fahimshariar28@gmail.com>
2024-12-16 22:01:17 +0600

    Header component used in the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/Header";
 import "./globals.css";
 
 export const metadata = {
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
     </html>
   );
