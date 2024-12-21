3caa9a02e252f2d689e3e91b49efb7009d174396
fahimshariar28 <fahimshariar28@gmail.com>
2024-12-21 16:39:49 +0600

    rootReducer and baseApi added to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+import { reducer } from "./rootReducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
