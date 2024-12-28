e64f2382376cb223e28adb8b0a785babc71e8e30
fahimshariar28 <fahimshariar28@gmail.com>
2024-12-28 12:26:31 +0600

    privacy policy and terms and conditions pages added

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="mt-16">
+      <h1 className="text-center">Privacy Policy</h1>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
---
+++ src/app/terms-and-conditions/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TermsCondition = () => {
+  return (
+    <div className="mt-16">
+      <h1 className="text-center">Terms and Conditions</h1>
+    </div>
+  );
+};
+
+export default TermsCondition;
