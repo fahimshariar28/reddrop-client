bbef42d3ece575baff5e6eb7d6823d3ffa81dfce
fahimshariar28 <fahimshariar28@gmail.com>
2024-12-31 02:48:02 +0600

    baseApi updated

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,8 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: () => ({}),
   tagTypes: [],
 });
