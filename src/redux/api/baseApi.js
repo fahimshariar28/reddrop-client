184ea84b33826f7c133067ef5609926b40cda2b8
fahimshariar28 <fahimshariar28@gmail.com>
2025-01-06 01:06:46 +0600

    baseApi updated

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -5,5 +5,5 @@ export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: () => ({}),
-  tagTypes: ["collaborator"],
+  tagTypes: ["collaborator", "request"],
 });
